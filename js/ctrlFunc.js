92a457fa5afe05e50551c2ca985e2a5935779f19
parkjunesung <wnstjd0842acm@naver.com>
2021-08-13 17:20:27 +0900

    수정

--- js/ctrlFunc.js
+++ js/ctrlFunc.js
@@ -1,12 +1,26 @@
-function sendSMS() {
-    var name = document.getElementById("name").value;
-    var phone = document.getElementById("phone").value;
-    var message = document.getElementById("message").value;
 
-    var url = "sms:";
-    url += phone + "?body=강서 서비스센터\n" +
-        "성함 : " + name + "\n" +
-        "내용 : " + message
+    function sendSMS() {
+        var name = document.getElementById("name").value;
+        var phone = document.getElementById("phone").value;
+        var message = document.getElementById("message").value;
+        var timestemp = new Date();
 
-    location.href = url;
-}
+        timestemp = timestemp.toLocaleString();
+        $.ajax({
+            type : "GET",
+            url : "https://script.google.com/macros/s/AKfycbwo3Tp80KoFMVCXbOo_sgzEr64AcTOYAcB8Ekhy6MefhLqNfn2ZLt3UHhN3ROcdrTTN/exec",
+            data : {
+                접수시간 : timestemp,
+                성함 : name,
+                연락처 : phone,
+                내용 : message,
+                처리상태 : '접수'
+            },
+            success : function(res){
+                $("#submitSuccessMessage").show();
+            },
+            error : function(XMLHttpRequest, textStatus, errorThrown){
+                $("#submitErrorMessage").show();
+            }
+        })
+    }
